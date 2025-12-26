67bcf9b6d0b8c124693f494ee7c0aa123cc105aa
Arthurlava <65039872+Arthurlava@users.noreply.github.com>
2025-12-26 19:52:05 +0100

    Add new item to items list in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -228,6 +228,7 @@ const WHATS_NEW = {
     "Offline multiplayer toegevoegd. multiplayer met punten op een apperaat!",
     "Solo Modus heeft nu ook punten!",
     "Check Woord knop toegevoegd voor als je niet zeker weet of een woord bestaat!",
+    "Dierenspel gefixed en zelfde rooms bekijken en joinen functionaliteit toegevoegd",
     "Fixed: Offline spelers worden niet meer geskipped!",
   ],
 };
